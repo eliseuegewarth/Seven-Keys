4dee3733d8cdf47885992c38a724b93e8b7ca707
egewarth <egewarth@gmail.com>
2016-04-29 06:07:15 -0300

    uncomplicating code of class ghost

--- Game/include/ghost.h
+++ Game/include/ghost.h
@@ -14,31 +14,38 @@ public:
     typedef enum { MOVED, STOPPED } Event;
     typedef enum { LEFT = 0, UP = 1, RIGHT = 2, DOWN = 3 } Direction;
 
-    Ghost(Object *parent, ObjectID id, double x, double y, int mass, bool walkable, string type, int dir);
+	#define GHOST_GUARD_SPEED_IN_MILISECONDS 60
+	#define GHOST_GUARD_SPEED_IN_MILISECONDS_HARD 120
+
+    Ghost(Object *parent, ObjectID id, const double x, const double y,
+		 const unsigned int mass_of_ghost_guard,
+		 const bool walkable, string type,
+		 const unsigned int initial_movement_direction);
     ~Ghost();
 
     Direction direction();
-    void get_playerx(int pos_x);
-    void get_playery(int pos_y);
-    void set_direction(Direction direction);
+    void get_playerx(const unsigned int player_horizontal_position);
+    void get_playery(const unsigned int player_vertical_position);
+    void set_direction(Direction direction_of_movement);
     void update_vision();
     void walk();
     void update_direction(unsigned long elapsed);
-    void change_animation(string path);
+    void change_animation();
 
     double damage();
 
 private:
     string type;
-    int player_posx;
-    int player_posy;
-    double m_damage;
+    int player_horizontal_position;
+    int player_vertical_position;
+    const double GHOST_GUARD_DAMAGE = 0.05;
+	const double GHOST_GUARD_SPEED = 0.6;
 
     void draw_self();
     void update_self(unsigned long elapsed);
-    unique_ptr<Animation> m_animation;
-    Direction m_direction;
-    unsigned long m_last = 0;
+    unique_ptr<Animation> ghost_guard_animation;
+    Direction direction_of_movement;
+    unsigned long last_game_time_saved = 0;
 };
 
 
--- Game/src/ghost.cpp
+++ Game/src/ghost.cpp
@@ -6,19 +6,51 @@
 
 #include <core/animation.h>
 
+#include <cassert>
 #include <iostream>
 
 using namespace std;
 
-Ghost::Ghost(Object *parent, ObjectID id, double x, double y, int mass, bool walkable, string t, int dir)
-    : Object(parent, id, x, y), type(t), m_damage(0.05), m_animation (new Animation("res/sprites/ghost_guarda1_running.png",
-    	0, 0, 70, 70, 8, 60, true)), m_direction((Direction) dir), m_last(0)
+const string& GHOST_GUARD_RUNNING_PATH = "res/sprites/ghost_guarda1_running.png";
+const string& GHOST_GUARD_3_RUNNING_PATH = "res/sprites/ghost_guarda3_running.png";
+
+/**
+ * [Ghost::Ghost Loads the guard information in the game]
+ * @param  parent                           [The parent is the map object that
+ *                                          will contains the ghost guard.]
+ * @param  id                               [identifier of object]
+ * @param  ghost_guard_horizontal_position  [Guard position on the map in the axis x]
+ * @param  ghost_guard_vertical_position    [Guard position on the map in the axis y]
+ * @param  mass_of_ghost_guard              [Contains mass information of the guard object]
+ * @param  walkable                         [Defines if the ghost guard is an object able to walk]
+ * @param  t                                [description]
+ * @param  initial_movement_direction       [Defines the initial direction of moviment]
+ */
+Ghost::Ghost(Object *parent, ObjectID id,
+            const double ghost_guard_horizontal_position,
+            const double ghost_guard_vertical_position,
+            const unsigned int mass_of_ghost_guard,
+            const bool walkable, string t,
+            const unsigned int initial_movement_direction)
 {
+    Object(parent, id, ghost_guard_horizontal_position,
+           ghost_guard_vertical_position);
+    type = t;
+
+    ghost_guard_animation = (unique_ptr<Animation>)new Animation
+    (GHOST_GUARD_RUNNING_PATH, 0, 0, 70, 70, 8,
+    GHOST_GUARD_SPEED_IN_MILISECONDS, true);
+    this->direction_of_movement = (Direction) initial_movement_direction;
+    last_game_time_saved = 0;
 
     if(t == "hard")
-       change_animation("res/sprites/ghost_guarda3_running.png");
-
-    this->set_mass(mass);
+    {
+        change_animation();
+    }else
+    {
+       //do nothing
+    }
+    this->set_mass(mass_of_ghost_guard);
     this->set_w(70);
     this->set_h(70);
     this->set_walkable(walkable);
@@ -29,106 +61,171 @@ Ghost::~Ghost()
 {
 }
 
-Ghost::Direction
-Ghost::direction()
+/**
+ * [Ghost::direction Just returns informations of m_direction.]
+ * @method Ghost::direction
+ * @return [description]
+ */
+Ghost::Direction Ghost::direction()
 {
-    return m_direction;
+    return direction_of_movement;
 }
 
-void
-Ghost::update_vision()
+void Ghost::update_vision()
 {
 }
 
-void
-Ghost::set_direction(Direction direction)
+/**
+ * [Ghost::set_direction Modifies the movement direction]
+ * @method Ghost::set_direction
+ * @param  direction_of_movement [The value of the direction that will be setted.]
+ */
+void Ghost::set_direction(Direction direction_of_movement)
 {
-    m_direction = direction;
+    this->direction_of_movement = direction_of_movement;
 }
 
-void
-Ghost::draw_self()
+/**
+ * [Ghost::draw_self Draws the animation of ghost guard on screen.]
+ * @method Ghost::draw_self
+ */
+void Ghost::draw_self()
 {
-    m_animation->draw(x(), y());
+    ghost_guard_animation->draw(x(), y());
 }
-
-void
-Ghost::walk()
+/**
+ * [Ghost::walk Walk to the player's position at a constant speed.]
+ * @method Ghost::walk
+ */
+void Ghost::walk()
 {
-    double speed = 0.6;
-    if(player_posx < this->x())
-        set_x(x() - speed);
-    else
-        set_x(x() + speed);
-
-    if(player_posy < this->y())
-        set_y(y() - speed);
-    else
-        set_y(y() + speed);
-
-    if(player_posx > this->x() - 100 && player_posx < this->x() + 100 && player_posy < this->y())
+    //
+    const bool player_is_to_the_right = (player_horizontal_position > this->x());
+    const bool player_is_to_the_left = (player_horizontal_position < this->x());
+    if(player_is_to_the_right)
+    {
+        set_x(x() - GHOST_GUARD_SPEED);
+    }else if(player_is_to_the_left)
+    {
+        set_x(x() + GHOST_GUARD_SPEED);
+    }else
+    {
+        set_x(x());
+    }
+    //
+    const bool player_is_to_the_buttom = (player_vertical_position < this->y());
+    const bool player_is_to_the_top = (player_vertical_position > this->y());
+    if(player_is_to_the_buttom)
+    {
+        set_y(y() - GHOST_GUARD_SPEED);
+    }else if(player_is_to_the_top)
+    {
+        set_y(y() + GHOST_GUARD_SPEED);
+    }else
+    {
+        set_y(y());
+    }
+    //
+    const bool player_is_aligned_vertically = (not player_is_to_the_right and not player_is_to_the_left);
+    if(player_is_aligned_vertically and player_is_to_the_top)
+    {
         set_direction(Ghost::UP);
-    else if(player_posx > this->x() - 100 && player_posx < this->x() + 100 && player_posy > this->y())
+    }else if(player_is_aligned_vertically and player_is_to_the_buttom)
+    {
         set_direction(Ghost::DOWN);
-    else if(player_posx < this->x())
+    }else if(player_is_to_the_left)
+    {
         set_direction(Ghost::LEFT);
-    else
+    }else if(player_is_to_the_right)
+    {
         set_direction(Ghost::RIGHT);
+    }else
+    {
+        
+    }
 }
-
-void
-Ghost::update_direction(unsigned long elapsed)
+/**
+ * [Ghost::update_direction Updates the sprite of object (ghost guard) based on the ghost guard movement direction.]
+ * @method Ghost::update_direction
+ * @param  elapsed                 [Elapsed time since the game start. This parameter is not used here.]
+ */
+void Ghost::update_direction(unsigned long elapsed)
 {
-    if(elapsed - m_last > 5000)
+    if(elapsed - last_game_time_saved > 5000)
     {
         int random = rand()%100;
 
         if(random < 25)
+        {
             set_direction(Ghost::LEFT);
-        else if(random < 50)
+        }else if(random < 50)
+        {
             set_direction(Ghost::UP);
-        else if(random < 75)
+        }else if(random < 75)
+        {
             set_direction(Ghost::RIGHT);
-        else
+        }else
+        {
             set_direction(Ghost::DOWN);
+        }
 
-        m_last = elapsed;
+        last_game_time_saved = elapsed;
     }
-    m_animation->set_row(this->direction());
+    ghost_guard_animation->set_row(this->direction());
 }
 
-void
-Ghost::get_playerx(int pos_x)
+/**
+ * [Ghost::get_playerx Receive the player's horizontal position.
+ * This information is used at Ghost Guard artificial intelligence.]
+ * @method Ghost::get_playerx
+ * @param  player_horizontal_position [That's the player's horizontal position.]
+ */
+void Ghost::get_playerx(const unsigned int player_horizontal_position)
 {
-    player_posx = pos_x;
+    this->player_horizontal_position = player_horizontal_position;
 }
-
-void
-Ghost::get_playery(int pos_y)
+/**
+ * [Ghost::get_playery Receive the player's vertical position.
+ * This information is used at Ghost Guard artificial intelligence.]
+ * @method Ghost::get_playery
+ * @param  player_vertical_position [That's the player's vertical position.]
+ */
+void Ghost::get_playery(const unsigned int player_vertical_position)
 {
-    player_posy = pos_y;
+    this->player_vertical_position = player_vertical_position;
 }
-
-double
-Ghost::damage()
+/**
+ * [Ghost::damage Returns the ghost guard attack damage value.]
+ * @method Ghost::damage
+ * @return [description]
+ */
+double Ghost::damage()
 {
-    return m_damage;
+    return GHOST_GUARD_DAMAGE;
 }
-
-void
-Ghost::update_self(unsigned long elapsed)
+/**
+ * [Ghost::update_self Updates the ghost guard position on the map and walk.]
+ * @method Ghost::update_self
+ * @param  elapsed            [Elapsed time since the game start.]
+ */
+void Ghost::update_self(unsigned long elapsed)
 {
-
     set_x(this->x());
     set_y(this->y());
-   
+
     update_direction(elapsed);
-    m_animation->update(elapsed);
+    ghost_guard_animation->update(elapsed);
     walk();
 }
-
-void
-Ghost::change_animation(string path)
+/**
+ * [Ghost::change_animation It is the animation of the ghost guard
+ * that will be loaded on the screen.]
+ * @method Ghost::change_animation
+ */
+void Ghost::change_animation()
 {
-    m_animation.reset(new Animation(path, 0, 0, 70, 70, 8, 120, true));
+    ghost_guard_animation.reset(new Animation(GHOST_GUARD_3_RUNNING_PATH,
+                                              0, 0, 70, 70, 8,
+                                              GHOST_GUARD_SPEED_IN_MILISECONDS_HARD,
+                                              true));
 }
