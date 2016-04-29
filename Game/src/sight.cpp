69f6891e8e3160d6a90b988ee541ec279332807d
egewarth <egewarth@gmail.com>
2016-04-29 06:58:00 -0300

    make assert from class sight

--- Game/src/sight.cpp
+++ Game/src/sight.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <core/animation.h>
 #include <core/environment.h>
@@ -5,8 +6,10 @@
 #include "sight.h"
 
 Sight::Sight(Object *parent, ObjectID id, double x, double y, double w, double h)
-    : Object(parent, id, x, y, w, h)
 {
+    assert((parent != NULL) && "Parent can't be NULL");
+    assert((not id.empty()) && "Id can't be empty");
+    Object(parent, id, x, y, w, h);
 }
 
 Sight::~Sight()
