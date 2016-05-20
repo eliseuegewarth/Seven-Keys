98b936d7bda23032e1a500f12caa43ae455980ce
lucas <lucaslrt@outlook.com>
2016-05-20 10:51:40 -0300

    Creating assertions for Item class.

--- Game/src/item.cpp
+++ Game/src/item.cpp
@@ -2,6 +2,7 @@
 #include <core/image.h>
 #include <core/keyboardevent.h>
 #include <core/environment.h>
+#include <cassert>
 
 #include "item.h"
 
@@ -9,6 +10,10 @@ Item::Item(Object *parent, ObjectID id, const string& path, double x,
  double y, double mass, bool walkable)
     : Image(parent, id, path)
 {
+    assert(parent != NULL && "Failed to pick up the instance of Object");
+    assert(ObjectID != NULL && "Failed to pick up the instance of ObjectID");
+    assert(path != NULL && "Path not found.");
+
     set_position(x, y);
     set_walkable(walkable);
     set_mass(mass);
