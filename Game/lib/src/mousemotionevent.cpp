61d6d5903ade2542e6fdd924bfa6a38cd84a846f
egewarth <egewarth@gmail.com>
2016-05-23 00:21:30 -0300

    comment class mouse motion event in javadoc style

--- Game/lib/src/mousemotionevent.cpp
+++ Game/lib/src/mousemotionevent.cpp
@@ -1,12 +1,16 @@
-/*
- * Implementação da classe que representa um evento de movimentação do mouse.
- *
- * Autor: Edson Alves
- * Data: 25/04/2015
- * Licença: LGPL. Sem copyright.
- */
 #include "core/mousemotionevent.h"
 
+/**
+ * [MouseMotionEvent::MouseMotionEvent description]
+ * @method MouseMotionEvent::MouseMotionEvent
+ * @param  x                    [Mouse position in axis x(horizontal)]
+ * @param  y                    [Mouse position in axis y(vertical)]
+ * @param  x_rel                [Mouse position in axis x(horizontal)]
+ * @param  y_rel                [Mouse position in axis y(vertical)]
+ * @param  left                 [Position where the mouse is moving (left)]
+ * @param  right                [Position where the mouse is moving (right)]
+ * @param  middle               [Position where the mouse is moving (middle)]
+ */
 MouseMotionEvent::MouseMotionEvent(int x, int y, int x_rel, int y_rel,
     MotionState left, MotionState right, MotionState middle)
 {
@@ -19,26 +23,39 @@ MouseMotionEvent::MouseMotionEvent(int x, int y, int x_rel, int y_rel,
     m_buttons[2] = middle;
 }
 
+/**
+ * [MouseMotionEvent::state State of the buttons, if it is pressed or not]
+ * @method MouseMotionEvent::state
+ * @param  button                  [Button that is pressed]
+ * @return                         [Return the button that is pressed]
+ */
 MouseMotionEvent::MotionState
 MouseMotionEvent::state(MouseMotionEvent::Button button) const
 {
     return m_buttons[button];
 }
 
-int
-MouseMotionEvent::x() const
+/**
+ * [MouseMotionEvent::x Mouse position in axis x(horizontal)]
+ * @method MouseMotionEvent::x
+ * @return [Returns the mouse position in axis x(horizontal)]
+ */
+int MouseMotionEvent::x() const
 {
     return m_x;
 }
 
-int
-MouseMotionEvent::y() const
+/**
+ * [MouseMotionEvent::y Mouse position in axis y(vertical)]
+ * @method MouseMotionEvent::y
+ * @return [Returns the mouse position in axis y(vertical)]
+ */
+int MouseMotionEvent::y() const
 {
     return m_y;
 }
 
-int
-MouseMotionEvent::x_rel() const
+int MouseMotionEvent::x_rel() const
 {
     return m_x_rel;
 }
@@ -49,8 +66,14 @@ MouseMotionEvent::y_rel() const
     return m_y_rel;
 }
 
-MouseMotionEvent
-MouseMotionEvent::from_SDL(const SDL_Event& event)
+/**
+ * [MouseMotionEvent::from_SDL      Reads the movements from the SDL library
+ * 									and returns the states for class]
+ * @method MouseMotionEvent::from_SDL
+ * @param  event                      [mouse event detected by the SDL library]
+ * @return                            [mouse movement]
+ */
+MouseMotionEvent MouseMotionEvent::from_SDL(const SDL_Event& event)
 {
     int x = event.motion.x;
     int y = event.motion.y;
