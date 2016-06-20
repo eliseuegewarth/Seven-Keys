d80fbcdfde5141a3f5769b88b1d0945096c7ce13
egewarth <egewarth@gmail.com>
2016-06-20 19:51:21 -0300

    fixing makefile to run tests

---
+++ Game/test/include/extras.hpp
@@ -0,0 +1,25 @@
+/**
+ * extras.hpp file
+ * @brief [brief description]
+ * @details [long description]
+ * @return [description]
+ */#ifndef EXTRAS_H
+#define EXTRAS_H
+
+#include <core/level.h>
+
+class Image;
+
+class Extras : public Level //Class that represents the extra option of the main menu of the game.
+{
+public:
+    Extras();
+    virtual ~Extras();
+
+    bool on_message(Object *sender, MessageID id, Parameters parameters); 
+
+private:
+    void draw_self();
+};
+
+#endif
