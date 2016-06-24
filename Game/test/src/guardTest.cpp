b21b037f7da8f82844bdc083ce4af6f5fdb4e1dd
lucaslrt <lucaslrt@outlook.com>
2016-06-24 00:59:10 -0300

    Refactoring makefile.

--- Game/test/src/guardTest.cpp
+++ Game/test/src/guardTest.cpp
@@ -1,66 +1,132 @@
-#include "guard.hpp"
+#include "../../include/guard.hpp"
+#include "../../include/room.hpp"
+#include "../../include/map.hpp"
 
-#include <cassert>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
 
 using namespace std;
-//using namespace Guard;
-/*
-class GuardTest : public Guard {
-public:
-    GuardTest(){
 
-    }
-    void get_player_horizontal_position_test()
+class TestGuard : public CppUnit::TestCase
     {
-        Guard *guard = new Guard(this, name, 0, 0, 60, false, type, randint(0,3));
-        int player_horizontal_position = 0;
-        Guard::get_player_horizontal_position(player_horizontal_position);
-        assert(this->player_horizontal_position == player_horizontal_position);
+
+      CPPUNIT_TEST_SUITE( TestGuard );
+      CPPUNIT_TEST( get_playerx_test );
+      //CPPUNIT_TEST( anotherExample );
+      //CPPUNIT_TEST( testEquals );
+      CPPUNIT_TEST_SUITE_END();
+
+      Guard * GuardTestObj;
+      Room * RoomTestObj;
+      Map * MapTestObj;
+
+    	void get_playerx_test ();
+    	//void			anotherExample ();
+    	//void			testEquals ();
+
+    public:
+
+    	void setUp ();
+      void tearDown();
+    };
+
+
+    CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( TestGuard, "TestGuard" );
+
+    void TestGuard::setUp () {
+
+        MapTestObj = new Map(4,1);
+        RoomTestObj = new Room(MapTestObj, "sala 0", "None", nullptr, nullptr, nullptr, nullptr, 1);
+        GuardTestObj =  new Guard(RoomTestObj, "guard", 0, 0, 60, false, "normal", 2);
+        printf("criou os objetos");
     }
-    void get_player_vertical_position_test()
+
+    void TestGuard::tearDown(){
+        if(GuardTestObj != NULL){
+            delete GuardTestObj;
+        }else{
+            //do nothing
+        }
+
+        if(RoomTestObj != NULL){
+            delete RoomTestObj;
+        }else{
+          //do nothing
+        }
+
+        if(MapTestObj != NULL){
+            delete MapTestObj;
+        }else{
+            //do nothing
+        }
+        printf("deletou os objetos");
+    }
+
+    void get_playerx_test()
     {
-        Guard *guard = new Guard(this, name, 0, 0, 60, false, type, randint(0,3));
-        int player_vertical_position = 0;
-        Guard::get_player_vertical_position(player_vertical_position);
-        assert(this->player_vertical_position == player_vertical_position);
+        CPPUNIT_ASSERT (1 == 1);
+        // int player_horizontal_position = 0;
+        // //Guard::get_playerx(player_horizontal_position);
+        // assert(true == true);
     }
-    void set_direction_test(Direction direction_of_movement)
-    {
-        Guard *guard = new Guard(this, name, 0, 0, 60, false, type, randint(0,3));
-        Direction direction_of_movement = Guard::RIGHT;
-        Guard::set_direction(direction_of_movement);
-        assert(this->direction_of_movement == direction_of_movement);
-    }
-    void walk_test()
-    {
-        Guard *guard = new Guard(this, name, 0, 0, 60, false, type, randint(0,3));
-        int player_horizontal_position = 0;
-        int player_vertical_position = 0;
-        Guard::get_player_horizontal_position(player_horizontal_position);
-        Guard::get_player_vertical_position(player_vertical_position);
-        Guard::walk();
-    }
-    void update_direction_test()
-    {
-        unsigned long elapsed = 5001; //condition that is accepted by the 'if' of the method
-        Guard *guard = new Guard(this, name, 0, 0, 60, false, type, randint(0,3));
-        Direction direction_of_movement_before_update = this->direction_of_movement;
-        Guard::update_direction(elapsed);
-        assert(this->direction_of_movement != direction_of_movement_before_update);
-    }
-    void damage_test()
-    {
-        Guard *guard = new Guard(this, name, 0, 0, 60, false, type, randint(0,3));
-        double Guard_damage;
-        Guard_damage = Guard::damage();
-        assert(Guard_damage == this->GUARD_DAMAGE);
-    }
-    void health()
-    {
-        Guard *guard = new Guard(this, name, 0, 0, 60, false, type, randint(0,3));
-        double guard_health_to_test;
-        guard_health_to_test = Guard::health();
-        assert(guard_health_to_test == this->guard_health);
-    }
-}
-*/
+    // void get_playery_test()
+    // {
+    //     setUp();
+    //     int player_vertical_position = 0;
+    //     Guard::get_playery(player_vertical_position);
+    //     assert(player_vertical_position == player_vertical_position);
+    // }
+    // void set_direction_test(Guard::Direction direction_of_movement)
+    // {
+    //     setUp();
+    //     Guard::Direction direction_of_movement = Guard::RIGHT;
+    //     Guard::set_direction(direction_of_movement);
+    //     assert(direction_of_movement == direction_of_movement);
+    // }
+    // void walk_test()
+    // {
+    //     setUp();
+    //     int player_horizontal_position = 0;
+    //     int player_vertical_position = 0;
+    //     Guard::get_playerx(player_horizontal_position);
+    //     Guard::get_playery(player_vertical_position);
+    //     Guard::walk();
+    // }
+    // void update_direction_test()
+    // {
+    //     setUp();
+    //     unsigned long elapsed = 5001; //condition that is accepted by the 'if' of the method
+    //     Direction direction_of_movement_before_update = direction_of_movement;
+    //     Guard::update_direction(elapsed);
+    //     assert(direction_of_movement != direction_of_movement_before_update);
+    // }
+    // void damage_test()
+    // {
+    //     setUp();
+    //     double Guard_damage;
+    //     Guard_damage = Guard::damage();
+    //     assert(Guard_damage == GUARD_DAMAGE);
+    // }
+    // void health()
+    // {
+    //     setUp();
+    //     double guard_health_to_test;
+    //     guard_health_to_test = Guard::health();
+    //     assert(guard_health_to_test == guard_health);
+    // }
+//};
+
+// int main(){
+//     TestGuard *testGuard = new TestGuard();
+//     testGuard->setUp();
+//     return 0;
+// }
+
+
+
+    /*
+
+    */
